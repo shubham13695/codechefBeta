c8fbad53e49389f635348fcbda5a95bf1998ed3f
shubhamsingh13695@gmail.com <shubhamsingh13695@gmail.com>
2018-09-01 19:34:07 +0530

    Shubham:minor changes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from '../component/home/home.component';
 
 
 export const AppRoutes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent }
   ];
 
