e9bd7d62906033e5915dd1304476d44835eb48fa
shubhamsingh13695@gmail.com <shubhamsingh13695@gmail.com>
2018-09-30 19:56:25 +0530

    Shubham:minor changes

--- src/component/problem/problem.component.ts
+++ src/component/problem/problem.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Authentication } from '../../service/authentication.service';
 import { AppSettings } from '../../config/app.config';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../service/user.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
     selector: 'app-problem',
@@ -9,13 +11,21 @@ import { ActivatedRoute } from '@angular/router';
     styleUrls: ['problem.component.css'],
 })
 export class ProblemComponent  {
-    constructor(private appSettings: AppSettings, private authentication: Authentication, private activatedroute: ActivatedRoute) {
+    problemDetails: string;
+    problemName: string;
+    authorName: string;
+    constructor(private appSettings: AppSettings, private authentication: Authentication, private activatedroute: ActivatedRoute,
+        private userservice: UserService , private spinner: NgxSpinnerService) {
         console.log('Test');
         this.authentication.get(this.appSettings.codeChefApiBaseUrl + 'contests/' + this.activatedroute.snapshot.params['contestcode'] +
-        '/problems/' + this.activatedroute.snapshot.params['problemcode'] , 'private').subscribe((data) => {
-            console.log(data);
+        '/problems/' + this.activatedroute.snapshot.params['problemcode'] , 'private').subscribe((data: any) => {
+            console.log(data.result);
+            this.problemDetails = data.result.data.content.body;
+            this.problemName = data.result.data.content.problemName;
+            this.authorName = data.result.data.content.author;
         }, (error) => {
-            console.log(error);
+            this.spinner.hide();
+            this.userservice.refreshToken(error);
         } );
     }
 }
--- src/component/problem/problem.module.ts
+++ src/component/problem/problem.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { ProblemComponent } from './problem.component';
+import {LayoutModule} from '../../layout/layout.module';
 
 @NgModule({
+    imports: [ LayoutModule ],
     declarations: [ ProblemComponent ],
 })
 export class ProblemModule {  }
